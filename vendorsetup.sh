940574347b686e4a9a0b367f919cb023e91405fd
Sonal Singh <sonal.singh.19993@gmail.com>
2023-11-16 18:45:10 +0530

    miatoll: Evolution-X Bringup
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,18 @@
+# Device Tree
+git clone https://github.com/Evolution-X-Miatoll/android_device_xiaomi_sm6250-common.git -b udc device/xiaomi/sm6250-common
+
+# Vendor Tree
+git clone https://github.com/Evolution-X-Miatoll/android_vendor_xiaomi_miatoll.git -b udc vendor/xiaomi/miatoll
+git clone https://github.com/Evolution-X-Miatoll/android_vendor_xiaomi_sm6250-common.git -b udc vendor/xiaomi/sm6250-common
+
+# Kernel Tree
+git clone https://github.com/Evolution-X-Miatoll/android_kernel_xiaomi_sm6250.git -b udc kernel/xiaomi/sm6250
+
+# MiuiCamera
+git clone https://gitlab.com/SonalSingh18/android_vendor_xiaomi_miuicamera.git -b fourteen vendor/xiaomi/miuicamera
+
+# ViPER4AndroidFX
+git clone https://github.com/Evolution-X-Miatoll/android_packages_apps_ViPER4AndroidFX.git -b master packages/apps/ViPER4AndroidFX
+
+# Timekeep
+git clone https://github.com/LineageOS/android_hardware_sony_timekeep.git -b lineage-21 hardware/sony/timekeep
