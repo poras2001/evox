fd178f3e4c6a5daf36108e2ba18ee89ad5777321
poras2001 <91818021+poras2001@users.noreply.github.com>
2023-12-22 08:25:56 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -41,3 +41,8 @@ PRODUCT_COPY_FILES += \
 
 # Inherit proprietary targets
 $(call inherit-product, vendor/xiaomi/miatoll/miatoll-vendor.mk)
+
+# Meme Camera
+TARGET_CAMERA_USES_NEWER_HIDL_OVERRIDE_FORMAT := true
+TARGET_INCLUDES_MIUI_CAMERA := true
+$(call inherit-product-if-exists, vendor/xiaomi/camera/miuicamera.mk)
